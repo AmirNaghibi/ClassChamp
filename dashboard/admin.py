67e88ecd4ab17645abb73a3627b2bcca791ae38b
Amir Naghibi <amirmahdi76@gmail.com>
2018-07-23 17:00:57 -0700

    registered models with admin and changed their presentation

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import Assessment, Course
+from .models import Assessment, Course, Grades
 
 # Register your models here.
-admin.site.register(Assessment)
+#admin.site.register(Assessment)
 admin.site.register(Course)
+#admin.site.register(Grades)
+
+
+# Register the Admin classe for Grades using the decorator
+@admin.register(Grades)
+class GradesAdmin(admin.ModelAdmin):
+    list_display = ('course', 'assessment_type', 'assessment_name','grade')
+    fields = ['course','assessment_type',('assessment_name','grade'),'date_added']
+
+
+# Register the Admin classe for Assessment using the decorator
+@admin.register(Assessment)
+class AssessmentAdmin(admin.ModelAdmin):
+    list_display = ('course_name', 'homeworks', 'quizzes','midterm','final')
