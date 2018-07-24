fbc401ec2fad46b52763d57243e82c75437df737
Amir Naghibi <amirmahdi76@gmail.com>
2018-07-24 16:54:38 -0700

    successfully query info

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,10 +8,11 @@ def index(request):
     """
     # Generate counts of some of the main objects
     #!num_books=Book.objects.all().count()
-    student_name = Student(last_name='naghibi').last_name
-    course_name = Course(name='CPSC 317').name
+    #student_name = Student.objects.get(last_name='naghibi').get_name()
+    student_name = Student.objects.get(last_name='naghibi').first_name
+    course_name = Course.objects.get(name='CPSC 317').name
     #assessment = Assessment.get_assessment()
-    homework_avg = 88#Grades.objects.filter(assessment_type__exact='h').avg()
+    homework_avg = Course.objects.get(name='CPSC 317').avg#Grades.objects.filter(assessment_type__exact='h').avg()
     #!num_instances=BookInstance.objects.all().count()
     # Available books (status = 'a')
     #!num_instances_available=BookInstance.objects.filter(status__exact='a').count()
