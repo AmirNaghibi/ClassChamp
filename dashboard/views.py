8905e27babff12e1bc7976e8aff6ce9ba2422ad8
Amir Naghibi <amirmahdi76@gmail.com>
2018-07-25 23:51:34 -0700

    calculate grade average logic

--- dashboard/views.py
+++ dashboard/views.py
@@ -10,12 +10,24 @@ def index(request):
     """
     student_name = Student.objects.get(last_name='naghibi').first_name
     course_name = Course.objects.get(name='CPSC 317').name
-    homework_avg = Grades.objects.all().filter(assessment_type='h').aggregate(Avg('grade'))['grade__avg'] #Grades.objects.filter(assessment_type__exact='h').avg()
+    
+    # average grade calculations: homework, quiz, midterm
+    hw_avg = Grades.objects.all().filter(assessment_type='h').aggregate(Avg('grade'))['grade__avg']
+    
+    quizz = Grades.objects.all().filter(assessment_type='q').aggregate(Avg('grade'))['grade__avg']
+    
+    mt_avg = Grades.objects.all().filter(assessment_type='m').aggregate(Avg('grade'))['grade__avg']
+    
+
+    hw_weight = 1 if (Grades.objects.all().filter(assessment_type='h').count()>0) else 0
+    qz_weight = 1 if (Grades.objects.all().filter(assessment_type='q').count()>0) else 0
+    mt_weight = 1 if (Grades.objects.all().filter(assessment_type='m').count()>0) else 0
+    fn_weight = 1 if (Grades.objects.all().filter(assessment_type='f').count()>0) else 0
     
     
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
-        context={'student_name':student_name,'course_name':course_name,'homework_avg':homework_avg},
+        context={'student_name':student_name,'course_name':course_name,'homework_avg':hw_avg},
     )
