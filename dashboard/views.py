14dd0f6d99b681a355d618b1e92fccd34b9e7329
Amir Naghibi <amirmahdi76@gmail.com>
2018-07-26 22:47:00 -0700

    process data to return avrg of course given course_name string

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,17 +9,17 @@ def index(request):
     View function for home page of site.
     """
     student_name = Student.objects.get(last_name='naghibi').first_name
-    course_name = Course.objects.get(name='CPSC 317').name
+    course_name = Course.objects.get(name='CPSC317').name
     # average grade calculations: homework, quiz, midterm
-    hw_avrg = 0 if (Grades.objects.all().filter(assessment_type='h').count()==0) else Grades.objects.all().filter(assessment_type='h').aggregate(Avg('grade'))['grade__avg']
-    qz_avrg = 0 if (Grades.objects.all().filter(assessment_type='q').count()==0) else Grades.objects.all().filter(assessment_type='q').aggregate(Avg('grade'))['grade__avg']
-    mt_avrg = 0 if (Grades.objects.all().filter(assessment_type='m').count()==0) else Grades.objects.all().filter(assessment_type='m').aggregate(Avg('grade'))['grade__avg']
-    fn_avrg = 0 if (Grades.objects.all().filter(assessment_type='f').count()==0) else Grades.objects.all().filter(assessment_type='f').aggregate(Avg('grade'))['grade__avg']
+    hw_avrg = 0 if (Grades.objects.all().filter(course__name=course_name,assessment_type='h').count()==0) else Grades.objects.all().filter(course__name=course_name,assessment_type='h').aggregate(Avg('grade'))['grade__avg']
+    qz_avrg = 0 if (Grades.objects.all().filter(course__name=course_name,assessment_type='q').count()==0) else Grades.objects.all().filter(course__name=course_name,assessment_type='q').aggregate(Avg('grade'))['grade__avg']
+    mt_avrg = 0 if (Grades.objects.all().filter(course__name=course_name,assessment_type='m').count()==0) else Grades.objects.all().filter(course__name=course_name,assessment_type='m').aggregate(Avg('grade'))['grade__avg']
+    fn_avrg = 0 if (Grades.objects.all().filter(course__name=course_name,assessment_type='f').count()==0) else Grades.objects.all().filter(course__name=course_name,assessment_type='f').aggregate(Avg('grade'))['grade__avg']
     # weight of assessments
-    hw_weight = 0 if (Grades.objects.all().filter(assessment_type='h').count()==0) else Assessment.objects.all().get(course_name='cpsc 317').homeworks
-    qz_weight = 0 if (Grades.objects.all().filter(assessment_type='q').count()==0) else Assessment.objects.all().get(course_name='cpsc 317').quizzes
-    mt_weight = 0 if (Grades.objects.all().filter(assessment_type='m').count()==0) else Assessment.objects.all().get(course_name='cpsc 317').midterms
-    fn_weight = 0 if (Grades.objects.all().filter(assessment_type='f').count()==0) else Assessment.objects.all().get(course_name='cpsc 317').final
+    hw_weight = 0 if (Grades.objects.all().filter(course__name=course_name,assessment_type='h').count()==0) else Course.objects.all().get(name=course_name).assessment.homeworks
+    qz_weight = 0 if (Grades.objects.all().filter(course__name=course_name,assessment_type='q').count()==0) else Course.objects.all().get(name=course_name).assessment.quizzes
+    mt_weight = 0 if (Grades.objects.all().filter(course__name=course_name,assessment_type='m').count()==0) else Course.objects.all().get(name=course_name).assessment.midterms
+    fn_weight = 0 if (Grades.objects.all().filter(course__name=course_name,assessment_type='f').count()==0) else Course.objects.all().get(name=course_name).assessment.final
     # weighted average of the course based on grades achieved up to now
     course_grade_now = ((hw_avrg*hw_weight)+(qz_avrg*qz_weight)+(mt_avrg*mt_weight)+(fn_avrg*fn_weight))/(hw_weight+qz_weight+mt_weight+fn_weight)
 
