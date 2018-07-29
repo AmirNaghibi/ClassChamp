01e952f57955a37a28660723caeaf070c19d435c
Amir Naghibi <amirmahdi76@gmail.com>
2018-07-29 10:37:49 -0700

    implemented avg function

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,14 +2,8 @@ from django.shortcuts import render
 from .models import Student, Assessment, Course, Grades
 from django.db.models import Avg
 
-# Create your views here.
-
-def index(request):
-    """
-    View function for home page of site.
-    """
-    student_name = Student.objects.get(last_name='naghibi').first_name
-    course_name = Course.objects.get(name='CPSC317').name
+# calculate avg given course name
+def calculate_overall_avg(course_name):
     # average grade calculations: homework, quiz, midterm
     hw_avrg = 0 if (Grades.objects.all().filter(course__name=course_name,assessment_type='h').count()==0) else Grades.objects.all().filter(course__name=course_name,assessment_type='h').aggregate(Avg('grade'))['grade__avg']
     qz_avrg = 0 if (Grades.objects.all().filter(course__name=course_name,assessment_type='q').count()==0) else Grades.objects.all().filter(course__name=course_name,assessment_type='q').aggregate(Avg('grade'))['grade__avg']
@@ -21,13 +15,23 @@ def index(request):
     mt_weight = 0 if (Grades.objects.all().filter(course__name=course_name,assessment_type='m').count()==0) else Course.objects.all().get(name=course_name).assessment.midterms
     fn_weight = 0 if (Grades.objects.all().filter(course__name=course_name,assessment_type='f').count()==0) else Course.objects.all().get(name=course_name).assessment.final
     # weighted average of the course based on grades achieved up to now
-    course_grade_now = ((hw_avrg*hw_weight)+(qz_avrg*qz_weight)+(mt_avrg*mt_weight)+(fn_avrg*fn_weight))/(hw_weight+qz_weight+mt_weight+fn_weight)
+    overall_avg = ((hw_avrg*hw_weight)+(qz_avrg*qz_weight)+(mt_avrg*mt_weight)+(fn_avrg*fn_weight))/(hw_weight+qz_weight+mt_weight+fn_weight)
+    return overall_avg
+
+def index(request):
+    """
+    View function for home page of site.
+    """
+    student_name = Student.objects.get(last_name='naghibi').first_name
+    course_name = Course.objects.get(name='CPSC317').name
+    # weighted average of the course based on grades achieved up to now
+    course_grade_now = calculate_overall_avg(course_name)
 
  
     context = {
         'student_name':student_name,
         'course_name':course_name,
-        'overall_avg':round(course_grade_now,2),
+        'overall_avg':round(course_grade_now,3),
     }
     
     # Render the HTML template index.html with the data in the context variable
