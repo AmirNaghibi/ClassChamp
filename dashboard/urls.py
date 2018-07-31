0ad5d3019e0b5c283c025609683d98b60e8be1a9
Amir Naghibi <amirmahdi76@gmail.com>
2018-07-31 12:42:05 -0700

    added url pattern

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     # path('courses/', views.CourseListView.as_view(), name='courses'),
     path('courses/', views.coursesPage, name='courses'),
-    path('course/<int:pk>', views.course_detail_view, name='course-detail'),
+    path('courses/<int:pk>', views.course_detail_view, name='course-detail'),
 ]
